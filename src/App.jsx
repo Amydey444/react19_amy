ffc3d5dcd34ef7e2c5031f0abfdab349bb2b1cd8
Amydey444 <162399160+Amydey444@users.noreply.github.com>
2025-01-29 21:20:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { RouterProvider } from "react-router";
+import {createBrowserRouter,} from "react-router-dom";
+import { AppLayout } from "./components/layout/AppLayout";
+
+import "./App.css";
+
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Country } from "./pages/Country";
+import { Contact } from "./pages/Contact";
+import { ErrorPage } from "./pages/Errorpage";
+import { CountryDetails } from "./components/layout/CountryDetails";
+
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<ErrorPage />,
+    children:[
+      {
+        path: "/",
+        element: <Home />, // Default route
+      },
+      {
+        path:"about",
+        element:<About />,
+        },
+      {
+        path:"country",
+        element:<Country />,
+      },
+      {
+        path:"country/:id",
+        element:<CountryDetails />,
+      },
+      {
+        path:"contact",
+        element:<Contact />,
+      },
+      
+    ],
+  },
+  
+]);
+
+const App=() => {
+  return <RouterProvider router={router}></RouterProvider>
+};
+ export default App;
